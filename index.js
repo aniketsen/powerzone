dd1fada9d57477828b0f08e4a5708a84abdb5dee
aniketsen <41874564+aniketsen@users.noreply.github.com>
2018-10-06 10:53:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -17,10 +17,27 @@ if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 document.getElementById("header").style.height="3em";
 document.getElementById("title2").style.opacity="0";
 document.getElementById("social").style.opacity="0";
-document.getElementById("menu").style.top="3em";}
+document.getElementById("menu").style.top="3em";
+document.getElementById("services").style.top="4.5em";}
 else{
 document.getElementById("header").style.height="6.25em";
 document.getElementById("title2").style.opacity="1";
 document.getElementById("social").style.opacity="1";
-document.getElementById("menu").style.top="6.25em";}
+document.getElementById("menu").style.top="6.25em";
+document.getElementById("services").style.top="6.5em";}
 }
+
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "abc" && password == "abc123"){
+document.getElementById("logsign").style.opacity="0";
+document.getElementById("logsign").style.visibility="hidden";
+document.getElementById("user").innerHTML=`Welcome ${username}`;
+}
+else{
+alert("Wrong Username or Password. Try Again.");
+}
+}
+
+
