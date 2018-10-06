307b7f8a51681f47ac7b59c96dde90df5da44c9c
aniketsen <41874564+aniketsen@users.noreply.github.com>
2018-10-06 10:56:32 +0530

    Add files via upload

--- Contact_Us/index.js
+++ Contact_Us/index.js
@@ -5,16 +5,18 @@ if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 document.getElementById("header").style.height="3em";
 document.getElementById("title2").style.opacity="0";
 document.getElementById("social").style.opacity="0";
-document.getElementById("menu").style.top="3em";}
+document.getElementById("menu").style.top="3em";
+document.getElementById("services").style.top="4.5em";}
 else{
 document.getElementById("header").style.height="6.25em";
 document.getElementById("title2").style.opacity="1";
 document.getElementById("social").style.opacity="1";
-document.getElementById("menu").style.top="6.25em";}
+document.getElementById("menu").style.top="6.25em";
+document.getElementById("services").style.top="6.5em";}
 }
 
 function initMap() {
-var uluru = {lat: 12.9374799, lng: 77.5731221};
+var uluru = {lat: 12.9373613, lng: 77.5718497};
 var map = new google.maps.Map(document.getElementById('map'), {zoom: 17, center: uluru, gestureHandling: 'cooperative'});
 var marker = new google.maps.Marker({position: uluru, map: map});
 }
